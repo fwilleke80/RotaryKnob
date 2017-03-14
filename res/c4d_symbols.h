1db1c95f7f23460d25edb462c53e5aca2168ae0a
Frank Willeke <frank.willeke@laubwerk.com>
2017-03-14 22:41:35 +0100

    Added resource files

---
+++ res/c4d_symbols.h
@@ -0,0 +1,14 @@
+enum
+{
+	// string table definitions
+	IDS_TESTOBJECT	= 10000,
+
+	// Other
+	IDS_CUSTOMGUISTRING,
+	IDS_CUSTOMGUI_DOTS,
+	IDS_CUSTOMDATATYPE_DOTS,
+	IDS_CUSTOMGUI_ROTARYKNOB,
+
+	// End of symbol definition
+	_DUMMY_ELEMENT_
+};
---
+++ res/description/otest.h
@@ -0,0 +1,11 @@
+#ifndef OTEST_H__
+#define OTEST_H__
+
+enum
+{
+	TEST_PARAM_1   = 10000,
+	TEST_PARAM_2   = 10001,
+	TEST_PARAM_3   = 10002
+};
+
+#endif // OTEST_H__
