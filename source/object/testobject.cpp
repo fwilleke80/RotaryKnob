afd0d6d3c2015ad7d6821680c662cc39896d33ac
Frank Willeke <frank.willeke@laubwerk.com>
2017-03-14 22:39:30 +0100

    Added source code and for test object

---
+++ source/object/testobject.cpp
@@ -0,0 +1,42 @@
+#include "c4d.h"
+#include "c4d_symbols.h"
+#include "otest.h"
+#include "main.h"
+
+const Int32 ID_TESTOBJECT = 1038993;
+
+class TestObjectData : public ObjectData
+{
+	INSTANCEOF(TestObjectData, ObjectData)
+
+public:
+	virtual Bool Init(GeListNode* node);
+
+	static NodeData* Alloc();
+};
+
+
+Bool TestObjectData::Init(GeListNode* node)
+{
+	BaseObject *op = static_cast<BaseObject*>(node);
+	BaseContainer *data = op->GetDataInstance();
+	if (!data)
+		return false;
+
+	data->SetFloat(TEST_PARAM_1, 0.50);
+	data->SetFloat(TEST_PARAM_2, 2.25);
+	data->SetFloat(TEST_PARAM_3, 3.75);
+
+	return true;
+}
+
+NodeData *TestObjectData::Alloc()
+{
+	return NewObjClear(TestObjectData);
+}
+
+
+Bool RegisterTestObject()
+{
+	return RegisterObjectPlugin(ID_TESTOBJECT, GeLoadString(IDS_TESTOBJECT), 0, TestObjectData::Alloc, "Otest", AutoBitmap("otest.tif"), 0);
+}
